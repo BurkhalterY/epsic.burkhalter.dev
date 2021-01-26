a9fa4d627906c30f4943171c83541e1d98aef72c
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2021-01-26 18:23:46 +0100

    Update todo.php

--- application/views/home/todo.php
+++ application/views/home/todo.php
@@ -1,4 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<h2>Prioritaire</h2>
+<ul>
+	<li>HTML, CSS, Javascript (CIE 101)</li>
+	<li>Python + Flask (Module 104)</li>
+	<li>Module 114 - Partie 4</li>
+</ul>
+
+<h2>Modules</h2>
 <table class="table">
 	<tr><th>Année</th><th>Module</th><th>Description courte</th><th>DA</th><th>TS</th><th>IE</th><th>À faire</th></tr>
 	<tr class="table-success"><td>1</td><td>100</td><td>BDD (Access)</td><td>x</td><td>x</td><td>x</td><td></td></tr>
@@ -13,21 +22,21 @@
 	<tr class="table-danger"><td>1 (CIE)</td><td>302</td><td>Office</td><td>x</td><td>x</td><td>x</td><td>à faire</td></tr>
 	<tr class="table-danger"><td>1 (CIE)</td><td>304</td><td>Hardware</td><td>x</td><td>x</td><td>x</td><td>à faire</td></tr>
 	<tr class="table-danger"><td>1 (CIE)</td><td>305</td><td>Software</td><td>x</td><td>x</td><td>x</td><td>à faire</td></tr>
-	<tr><td>2</td><td>122</td><td>Bash</td><td>x</td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>126</td><td>Synology DSM</td><td></td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>129</td><td>Réseau</td><td></td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>143</td><td>RAID</td><td></td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>153</td><td>Modélisation BDD</td><td>x</td><td></td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>213</td><td>Esprit d'équipe</td><td>x</td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>214</td><td>Intruire les utilisateurs</td><td>x</td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>226a</td><td>POO (Java ou C#)</td><td>x</td><td></td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>266b</td><td>POO (Java ou C#)</td><td>x</td><td></td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>306</td><td>Gestion de projet</td><td>x</td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>426</td><td>Méthodes agiles</td><td>x</td><td></td><td></td><td>à faire</td></tr>
-	<tr><td>2</td><td>437</td><td>Support</td><td></td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2 (CIE)</td><td>105</td><td>SQL</td><td>x</td><td></td><td></td><td>à faire</td></tr>
-	<tr><td>2 (CIE)</td><td>127</td><td>Serveur</td><td></td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2 (CIE)</td><td>130</td><td>Réseau</td><td></td><td>x</td><td></td><td>à faire</td></tr>
-	<tr><td>2 (CIE)</td><td>318</td><td>C#</td><td>x</td><td></td><td></td><td>à faire</td></tr>
-	<tr><td>2 (CIE)</td><td>340</td><td>Virtualisation</td><td></td><td>x</td><td></td><td>à faire</td></tr>
+	<tr><td>2</td><td>122</td><td>Bash</td><td>x</td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>126</td><td>Synology DSM</td><td></td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>129</td><td>Réseau</td><td></td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>143</td><td>RAID</td><td></td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>153</td><td>Modélisation BDD</td><td>x</td><td></td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>213</td><td>Esprit d'équipe</td><td>x</td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>214</td><td>Intruire les utilisateurs</td><td>x</td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>226a</td><td>POO (Java ou C#)</td><td>x</td><td></td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>266b</td><td>POO (Java ou C#)</td><td>x</td><td></td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>306</td><td>Gestion de projet</td><td>x</td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>426</td><td>Méthodes agiles</td><td>x</td><td></td><td>?</td><td>à faire</td></tr>
+	<tr><td>2</td><td>437</td><td>Support</td><td></td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2 (CIE)</td><td>105</td><td>SQL</td><td>x</td><td></td><td>?</td><td>à faire</td></tr>
+	<tr><td>2 (CIE)</td><td>127</td><td>Serveur</td><td></td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2 (CIE)</td><td>130</td><td>Réseau</td><td></td><td>x</td><td>?</td><td>à faire</td></tr>
+	<tr><td>2 (CIE)</td><td>318</td><td>C#</td><td>x</td><td></td><td>?</td><td>à faire</td></tr>
+	<tr><td>2 (CIE)</td><td>340</td><td>Virtualisation</td><td></td><td>x</td><td>?</td><td>à faire</td></tr>
 <table>
