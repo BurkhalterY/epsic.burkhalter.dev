5c8c424e7a916dff85dccf357b150fde9044f9f1
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2021-02-04 12:22:45 +0100

    Fix screenshot url

--- application/views/access/access1.php
+++ application/views/access/access1.php
@@ -3,5 +3,5 @@
 <p>Installation d'Access 1.0 sur Windows 3.1</p>
 <iframe width="720" height="480" src="https://www.youtube.com/embed/6fbgRjiuYKs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p>Le formulaire F_Personnes de ma bdd dans Access 1.0 :</p>
-<img src="<?=base_url('medias/assecc/access1_screenshot.png')?>" alt="Capture d'écran" />
+<img src="<?=base_url('medias/access/access1_screenshot.png')?>" alt="Capture d'écran" />
 <ul><li><p>La VM est téléchargeable <a href="<?=base_url('medias/access/Windows3.ova')?>">ici</a>.</p></li></ul>
