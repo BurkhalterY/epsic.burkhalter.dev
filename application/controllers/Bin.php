03c8daa18ff37bb7df5f9d6153d4cc9e3778890e
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2021-02-04 17:42:35 +0100

    Test autogenerated image

--- application/controllers/Bin.php
+++ application/controllers/Bin.php
@@ -9,6 +9,55 @@ class Bin extends MY_Controller {
 		$this->display_view('bin/index', ['title' => 'Menu']);
 	}
 
+	public function gen() {
+		$arr = [
+			"Maccaud est un connard mais c'est pour ça qu'on l'aime !",
+			"Yannis est un gros bg. Il a créé ce site : epsic.burkhalter.dev",
+		];
+
+		$setence = str_pad($arr[array_rand($arr)], 76, " ");
+		$text = "";
+		for($i = 0; $i < strlen($setence); $i++){
+			if($i % 4 == 0 && $i != 0){
+				$text .= "\n";
+			}
+			$text .= str_pad(decbin(ord($setence[$i])), 8, "0", STR_PAD_LEFT)." ";
+		}
+
+		$im = new Imagick();
+		$pixel = new ImagickPixel('black');
+		$draw = new ImagickDraw();
+
+		$im->newImage(500, 500, $pixel);
+		$draw->setFillColor('green');
+		$draw->setFont('consola.ttf');
+		$draw->setFontSize(24);
+		$im->annotateImage($draw, 20, 40, 0, $text);
+		$im->setImageFormat('png');
+
+		$controlPoints = array( 0, 0,
+								10, 10,
+
+								500, 0,
+								480, 30,
+
+								0, 500,
+								75, 490,
+
+								500, 500,
+								480, 390,
+		);
+		$im->distortImage(Imagick::DISTORTION_PERSPECTIVE, $controlPoints, false);
+
+		$i2 = new Imagick();
+		$i2->setOption('gradient:vector', '-50,10,50,0');
+		$i2->newPseudoImage(500, 500, 'gradient:grey');
+		$im->compositeImage($i2, Imagick::COMPOSITE_BLEND, 0, 0);
+
+		header("Content-Type: image/png");
+		echo $im;
+	}
+
 	public function n_to_10() {
 		$this->display_view('bin/n_to_10', ['title' => 'Conversion base n en base 10']);
 	}
