4e13ec957fa43790c1292e864451f829f726066c
Burkhalter Yannis <yannis.burkhalter@gmail.com>
2021-08-27 16:56:12 +0200

    Edit main menu
    - Move "lien utiles" to the bottom (no modal)
    - Add <wbr> on title for mobile view

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,7 +4,7 @@
 <script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
 <br>
 <div class="container">
-	<h1 class="text-uppercase text-center">epsic.burkhalter.dev</h1>
+	<h1 class="text-uppercase text-center">epsic<wbr>.burkhalter<wbr>.dev</h1>
 	<p>Bonjour, bienvenue sur mon site. Ici j'essaie de retranscrire et de réexpliquer les modules que nous voyons aux cours. Vous pouvez aussi poser vos questions en rejoigant le serveur Discord, ou y apporter vos idées d'amélioration. Je suis ouvert à toutes les questions donc n'hésitez pas. Il est également possible que je commette des erreurs, merci de me les signaler si vous en voyez. Le site ne contient pour le moment que certains modules de première année. Sur-ce, révisez bien 😃</p>
 	<div class="row">
 		<div class="col-md-4">
@@ -63,39 +63,20 @@
 			</a>
 			<br>
 		</div>
-		<div class="col-md-4">
-			<a href="" data-toggle="modal" data-target="#modal">
-				<img src="<?=base_url('medias/home/cursor.png')?>" alt="Liens utiles" width="75%" class="link" />
-				<figcaption>Liens utiles</figcaption>
-			</a>
-			<br>
-			<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="Liens utiles" aria-hidden="true">
-				<div class="modal-dialog modal-dialog-centered" role="document">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title">Liens utiles</h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-								<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						<div class="modal-body">
-							<ul>
-								<li class="icon-moodle"><a href="https://www.epsic.ch/secure/moodle/" target="_blank">Moodle de l'EPSIC</a></li>
-								<li class="icon-moodle"><a href="https://ict-moodle.ch/" target="_blank">Moodle exclusif aux CIE</a></li>
-								<li class="icon-discord"><a href="https://discord.gg/ry3XrTPUzQ" target="_blank">Mon serveur Discord (Normalux)</a></li>
-								<li class="icon-discord"><a href="https://discord.gg/NejUt35yur" target="_blank">Serveur Discord de L'ELITE 2.0</a></li>
-								<li class="icon-trash"><a href="https://epsic.jacktrash.ch/" target="_blank">Site de JackTrash</a></li>
-								<li class="icon-hyperplanning"><a href="https://www.portail.vd.ch/formation/hyperplanning/epsic/invite" target="_blank">Horaires EPSIC</a></li>
-							</ul>
-						</div>
-						<div class="modal-footer"></div>
-					</div>
-				</div>
-			</div>
-		</div>
 	</div>
 	<div class="row">
-		<div class="col-lg-6 offset-lg-6">
+		<div class="col-lg-6">
+			<h4>Liens utiles</h4>
+			<ul>
+				<li class="icon-moodle"><a href="https://www.epsic.ch/secure/moodle/" target="_blank">Moodle de l'EPSIC</a></li>
+				<li class="icon-moodle"><a href="https://ict-moodle.ch/" target="_blank">Moodle exclusif aux CIE</a></li>
+				<li class="icon-discord"><a href="https://discord.gg/ry3XrTPUzQ" target="_blank">Mon serveur Discord (Normalux)</a></li>
+				<li class="icon-discord"><a href="https://discord.gg/NejUt35yur" target="_blank">Serveur Discord de L'ELITE 2.0</a></li>
+				<li class="icon-trash"><a href="https://epsic.jacktrash.ch/" target="_blank">Site de JackTrash</a></li>
+				<li class="icon-hyperplanning"><a href="https://www.portail.vd.ch/formation/hyperplanning/epsic/invite" target="_blank">Horaires EPSIC</a></li>
+			</ul>
+		</div>
+		<div class="col-lg-6">
 			<h4>Fil d'actualité</h4>
 			<iframe src="<?=base_url('home/actualite')?>" width="100%" height="200px" frameBorder="0"></iframe>
 		</div>
