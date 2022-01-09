a258beaffa7e5336914487c4b578a06629d099a9
Burkhalter Yannis <33062656+BurkhalterY@users.noreply.github.com>
2022-01-10 00:37:00 +0100

    Ready for prod (?)

--- src/views/tools/QrCode.vue
+++ src/views/tools/QrCode.vue
@@ -2,7 +2,6 @@
 	<div>
 		<h1>Générateur de QR Code</h1>
 		<textarea class="w-100" v-model="qrValue"></textarea>
-		<br><br>
 		<div class="text-center" v-if="qrValue"><vue-qrcode :value="qrValue"/></div>
 	</div>
 </template>
