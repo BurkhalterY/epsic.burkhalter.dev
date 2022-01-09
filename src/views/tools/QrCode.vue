2ec469f578080012a7e0dfbd730cafcb39fb0944
Burkhalter Yannis <33062656+BurkhalterY@users.noreply.github.com>
2022-01-10 00:21:12 +0100

    Fix QR Code generation

--- src/views/tools/QrCode.vue
+++ src/views/tools/QrCode.vue
@@ -3,12 +3,12 @@
 		<h1>Générateur de QR Code</h1>
 		<textarea class="w-100" v-model="qrValue"></textarea>
 		<br><br>
-		<div class="text-center"><vue-qrcode :value="qrValue"/></div>
+		<div class="text-center" v-if="qrValue"><vue-qrcode :value="qrValue"/></div>
 	</div>
 </template>
 
 <script>
-	import VueQrcode from '@chenfengyuan/vue-qrcode';
+	import VueQrcode from '@chenfengyuan/vue-qrcode'
 
 	export default {
 		name: 'QrCode',
