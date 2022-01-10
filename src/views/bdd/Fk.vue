c8e4d1e1a3e7b83b02a1ad90b71ad672bf33ac90
Burkhalter Yannis <33062656+BurkhalterY@users.noreply.github.com>
2022-01-10 02:22:57 +0100

    Fix whitespace and orthograph [2]
    Pourquoi il a pas prit tous les fichiers ce fdp.?

--- src/views/bdd/Fk.vue
+++ src/views/bdd/Fk.vue
@@ -5,7 +5,7 @@
 		<img src="/medias/bdd/ajout_fk_1.png" alt="Table sexe" />
 		<p>Ainsi qu'un champ fk_sexe à la fin de ma table t_personne</p>
 		<img src="/medias/bdd/ajout_fk_2.png" alt="Champ fk_sexe" />
-		<p>Pour définir le FK, cliquez sur index du champs fk</p>
+		<p>Pour définir le FK, cliquez sur index du champ fk</p>
 		<img src="/medias/bdd/ajout_fk_3.png" alt="Définir l'ndex" />
 		<p>Un pop-up apparait, cliquez sur OK pour confirmer.</p>
 		<img src="/medias/bdd/ajout_fk_4.png" alt="Confirmer" />
@@ -18,7 +18,7 @@
 			<li>Le champ fk (ici fk_sexe)</li>
 			<li>La table à lier (ici t_sexe)</li>
 			<li>L'id (devrait se remplir tout seul donc pas besoin de toucher)</li>
-			<li>Cliquez sur Sauvegarder et votre relation est terminé</li>
+			<li>Cliquez sur Sauvegarder et votre relation est prête</li>
 		</ol>
 		<img src="/medias/bdd/ajout_fk_7.png" alt="Contraintes de clé étrangère" />
 	</div>
