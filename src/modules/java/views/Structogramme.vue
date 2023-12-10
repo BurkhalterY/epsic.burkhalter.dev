534f5badbca4325b2ad8b942b94214ab88d1f478
Yannis Burkhalter <yannis@burkhalter.dev>
2023-12-10 09:35:57 +0100

    [#54] https://epsic.burkhalter.dev/java/structogramme

--- src/modules/java/views/Structogramme.vue
+++ src/modules/java/views/Structogramme.vue
@@ -14,58 +14,100 @@ import ShowCode from "@/components/ShowCode.vue"
       </thead>
       <tbody>
         <tr>
-          <td><img src="/java/medias/nassi-main.png" alt="main" /></td>
+          <td>
+            <img
+              src="/java/medias/nassi-main.png"
+              alt="main"
+              class="min-w-max"
+            />
+          </td>
           <td>
             <ShowCode file="/java/code/nassi-main.java" lang="java" />
           </td>
         </tr>
         <tr>
           <td>
-            <img src="/java/medias/nassi-instruction.png" alt="instruction" />
+            <img
+              src="/java/medias/nassi-instruction.png"
+              alt="instruction"
+              class="min-w-max"
+            />
           </td>
           <td>
             <ShowCode file="/java/code/nassi-instruction.java" lang="java" />
           </td>
         </tr>
         <tr>
-          <td><img src="/java/medias/nassi-if.png" alt="if" /></td>
+          <td>
+            <img src="/java/medias/nassi-if.png" alt="if" class="min-w-max" />
+          </td>
           <td>
             <ShowCode file="/java/code/nassi-if.java" lang="java" />
           </td>
         </tr>
         <tr>
-          <td><img src="/java/medias/nassi-switch.png" alt="if" /></td>
+          <td>
+            <img
+              src="/java/medias/nassi-switch.png"
+              alt="if"
+              class="min-w-max"
+            />
+          </td>
           <td>
             <ShowCode file="/java/code/nassi-switch.java" lang="java" />
           </td>
         </tr>
         <tr>
-          <td><img src="/java/medias/nassi-while.png" alt="while" /></td>
+          <td>
+            <img
+              src="/java/medias/nassi-while.png"
+              alt="while"
+              class="min-w-max"
+            />
+          </td>
           <td>
             <ShowCode file="/java/code/nassi-while.java" lang="java" />
           </td>
         </tr>
         <tr>
-          <td><img src="/java/medias/nassi-do-while.png" alt="dpo while" /></td>
+          <td>
+            <img
+              src="/java/medias/nassi-do-while.png"
+              alt="dpo while"
+              class="min-w-max"
+            />
+          </td>
           <td>
             <ShowCode file="/java/code/nassi-do-while.java" lang="java" />
           </td>
         </tr>
         <tr>
-          <td><img src="/java/medias/nassi-for.png" alt="for" /></td>
+          <td>
+            <img src="/java/medias/nassi-for.png" alt="for" class="min-w-max" />
+          </td>
           <td>
             <ShowCode file="/java/code/nassi-for.java" lang="java" />
           </td>
         </tr>
         <tr>
-          <td><img src="/java/medias/nassi-methode-appel.png" alt="for" /></td>
+          <td>
+            <img
+              src="/java/medias/nassi-methode-appel.png"
+              alt="for"
+              class="min-w-max"
+            />
+          </td>
           <td>
             <ShowCode file="/java/code/nassi-methode-appel.java" lang="java" />
           </td>
         </tr>
         <tr>
           <td>
-            <img src="/java/medias/nassi-methode-declaration.png" alt="for" />
+            <img
+              src="/java/medias/nassi-methode-declaration.png"
+              alt="for"
+              class="min-w-max"
+            />
           </td>
           <td>
             <ShowCode
