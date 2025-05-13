9d1d5a33b4dc8e850750a1dfc6c09ce4e40162f2
Yannis Burkhalter <yannis@burkhalter.dev>
2025-05-13 23:01:17 +0200

    fix(BTCPay): Store ID

--- src/components/BTCPay.vue
+++ src/components/BTCPay.vue
@@ -86,7 +86,7 @@
     <input
       type="hidden"
       name="storeId"
-      value="2fb6bmWGswBhrzwwSMpEJfTQhZ9tvB3GXaW4jRP7JrP1"
+      value="3SVVi7o9AAWCFeeA39n7H8iw6K4BsG4hrQxJPW8CnNbj"
     />
     <input type="hidden" name="checkoutDesc" value="Tip" />
     <input
